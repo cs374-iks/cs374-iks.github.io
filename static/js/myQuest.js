fe706127d5dc16f4208aeff85d2bddc0741adf93
LaLlorona <chachaami66@gmail.com>
2019-04-30 11:53:44 +0900

    desigin change'

--- static/js/myQuest.js
+++ static/js/myQuest.js
@@ -9,17 +9,17 @@ let current_quest = [
     },
     {
         profile_img:"./static/images/people2.jpg",
-        quest_content: "Visit Statue of Liberty, and take photo",
-        place_img : "./static/images/table1.jpg",
-        place_name : "statue of liberty",
+        quest_content: "Go to Times Squre, and eat Mcdonald's",
+        place_img : "./static/images/table3.jpg",
+        place_name : "Times Square",
         quest_status : false,
         id : "2"
     },
     {
         profile_img:"./static/images/people3.jpg",
-        quest_content: "Visit Statue of Liberty, and take photo",
-        place_img : "./static/images/table1.jpg",
-        place_name : "statue of liberty",
+        quest_content: "Visit The Metroplitan museum ",
+        place_img : "./static/images/table4.jpg",
+        place_name : "metropolitan museum",
         quest_status : false,
         id : "3"
     }
@@ -34,7 +34,7 @@ let profile= [
     }
     ,
     {
-        profile_img:"./static/images/people3.jpg",
+        profile_img:"./static/images/people1.jpg",
         quest_content: "Visit Statue of Liberty, and take photo",
         place_img : "./static/images/table1.jpg",
         place_name : "statue of liberty",
@@ -42,9 +42,9 @@ let profile= [
         id : "1"
     },
     {
-        profile_img:"./static/images/people3.jpg",
+        profile_img:"./static/images/people1.jpg",
         quest_content: "Visit Broadway, and choose one musical and watch it!",
-        place_img : "./static/images/table1.jpg",
+        place_img : "./static/images/table2.jpg",
         place_name : "Broadway",
         quest_status: false,
         id : "4"
@@ -58,6 +58,7 @@ let google_map = document.getElementById('map');
 let remove_quest_button = document.getElementById('remove_quest_button');
 let submit_quest_button = document.getElementById('submit_quest_button');
 let profile_quest_table = document.getElementById('quest_on_profile');
+let profile_instruction = document.getElementById('profile_instruction');
 
 function fillContent(divObj, content) {
     divObj.innerHTML = content;
@@ -83,28 +84,33 @@ function addAllContentsToTable() {
 
 
         if (current_quest[i].quest_status) { //when quest is completed
-            col1.innerHTML = `<img src=${current_quest[i].profile_img} width = 100em alt="donald trump">`;
+            col1.innerHTML = `<img src=${current_quest[i].profile_img} width = 50em height = 50em alt="donald trump" data-target = "#modal_profile" data-toggle = "modal" role = 'button' onclick="clickProfile()" > `;
 
             col2.innerHTML = `${current_quest[i].quest_content}`;
 
-            col3.innerHTML = `<img src=${current_quest[i].place_img} width = 100em alt="donald trump">`
+            col3.innerHTML = `<img src=${current_quest[i].place_img} width = 50em height = 50em alt="donald trump" data-target = "#modal_map" data-toggle = "modal" role = 'button' onclick="updateGoogleMap('${current_quest[i].place_name}')">`
 
-            col4.innerHTML = `quest complete!`
+
+            col4.innerHTML =
+                `Quest complete!`
 
         } else { //when quest is not completed
-            col1.innerHTML = `<img src=${current_quest[i].profile_img} width = 100em alt="donald trump" data-target = "#modal_profile" data-toggle = "modal" role = 'button' onclick="clickProfile()" > `;
+            col1.innerHTML = `<img src=${current_quest[i].profile_img} width = 50em height = 50em alt="donald trump" data-target = "#modal_profile" data-toggle = "modal" role = 'button' onclick="clickProfile()" > `;
 
             col2.innerHTML = `${current_quest[i].quest_content}`;
 
-            col3.innerHTML = `<img src=${current_quest[i].place_img} width = 100em alt="donald trump">`
+            col3.innerHTML = `<img src=${current_quest[i].place_img} width = 50em height = 50em alt="donald trump" data-target = "#modal_map" data-toggle = "modal" role = 'button' onclick="updateGoogleMap('${current_quest[i].place_name}')">`
 
-            //col4.innerHTML = `<a data-target = "#modal_map" data-toggle = "modal" role = 'button' onclick=`+"updateGoogleMap(${current_quest[i].place_name});>press here </a>"
 
-            col4.innerHTML = `<a data-target = "#modal_map" data-toggle = "modal" role = 'button' onclick="updateGoogleMap('${current_quest[i].place_name}');">
-                        <img src="./static/images/quest_map.png" width = 100em alt="map icon">
+            col4.innerHTML =
+                    `
+                    <a class = "btn btn-outline-success" data-target = "#modal_finish_quest" data-toggle = "modal" role = 'button' 
+                    onclick = "setOnclickOfButton(submit_quest_button,'completeQuest(${current_quest[i].id})')" style = "font-size : 10px; width:70px" >Complete
+                    
                     </a>
-                    <a class = "btn btn-success" data-target = "#modal_finish_quest" data-toggle = "modal" role = 'button' onclick = "setOnclickOfButton(submit_quest_button,'completeQuest(${current_quest[i].id})')" >I finish my quest!</a>
-                    <a class="btn btn-danger" data-target = "#modal_quit_quest" data-toggle = "modal" role = 'button' onclick="setOnclickOfButton(remove_quest_button,'removeById(${current_quest[i].id})')">Quit quest </a>`
+                    <p></p>
+                    <a class="btn btn-outline-danger" data-target = "#modal_quit_quest" data-toggle = "modal" role = 'button' 
+                    onclick="setOnclickOfButton(remove_quest_button,'removeById(${current_quest[i].id})')" style = "font-size : 10px; width: 70px" >Quit</a>`
 
         }
     }
@@ -140,6 +146,14 @@ function clickProfile(){ //function when click profile image
 
     let numRow = profile_quest_table.rows.length;
 
+    profile_instruction.innerHTML = `<img src="${profile[0].quest_provider_picture}" class="mr-3" style="height:128px; width:128px; overflow:hidden;">
+                    <div class="media-body">
+                      <h6 class="mt-0">
+                        ${profile[0].quest_provider_name}
+                      </h6>
+                      <p>${profile[0].quest_provider_introduction}</p>
+                    </div>`
+
     for (let i = 0; i < numRow ; i++){
         profile_quest_table.deleteRow(0);
     }
@@ -149,13 +163,12 @@ function clickProfile(){ //function when click profile image
         let col1 = row.insertCell(0);
         let col2 = row.insertCell(1);
         let col3 = row.insertCell(2);
-        let col4 = row.insertCell(3);
+
         let alreadyAdded = false;
-        col1.innerHTML = `<img src=${profile[0].quest_provider_picture} width = 100em alt="donald trump">`;
 
-        col2.innerHTML = `${profile[i].quest_content}`;
+        col1.innerHTML = `<li>${profile[i].quest_content}</li>`;
 
-        col3.innerHTML = `<img src=${profile[i].place_img} width = 100em alt="donald trump">`
+        col2.innerHTML = `<img src=${profile[i].place_img} width = 70em height = 70em alt="donald trump">`
 
         //col4.innerHTML = `<a data-target = "#modal_map" data-toggle = "modal" role = 'button' onclick=`+"updateGoogleMap(${profile[i].place_name});>press here </a>"
 
@@ -165,10 +178,10 @@ function clickProfile(){ //function when click profile image
             }
         }
         if (alreadyAdded){
-            col4.innerHTML = `<button id="button_${profile[i].id}" onclick = addThisQuest('${profile[i].id}') disabled = true>Add Quest</button>`
+            col3.innerHTML = `<button id="button_${profile[i].id}" class = 'btn btn-outline-warning' onclick = addThisQuest('${profile[i].id}') disabled = true style = 'font-size:10px'>Already Added</button>`
         }
         else{
-            col4.innerHTML = `<button id="button_${profile[i].id}" onclick = addThisQuest('${profile[i].id}')>Add Quest</button>`
+            col3.innerHTML = `<button id="button_${profile[i].id}" class = 'btn btn-outline-primary' onclick = addThisQuest('${profile[i].id}') style = 'font-size:10px'>Add Quest</button>`
         }
 
 
@@ -184,6 +197,8 @@ function addThisQuest(id){
             current_quest.push(profile[i])
             let disable_button = document.getElementById(`button_${profile[i].id}`);
             disable_button.disabled = true;
+            disable_button.class = 'btn btn-outline-warning';
+            disable_button.innerHTML = "Already Added"
 
         }
 
