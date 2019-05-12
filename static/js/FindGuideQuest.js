a0c8735e23dffca891415f3593cb85119e4063ca
river <river0105@naver.com>
2019-05-12 20:17:39 +0900

    onclick bug

--- static/js/FindGuideQuest.js
+++ static/js/FindGuideQuest.js
@@ -13,9 +13,12 @@
  *  */
 
 var guides = []; // guides represented as their ids
+var carousel_images_count = [3, 3, 2, 2];
 
-var searchButton = document.getElementById("search");
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchInput");
 var guideAccordion = document.getElementById("guideAccordion");
+var subtitle = document.getElementById("subtitle");
 
 /**
  * initialization function
@@ -24,13 +27,14 @@ $( document ).ready(function() {
     function init() {
         initGuideList();
         viewGuideList();
+        bindEvents();
     }
     
     /**
      * initialize top-rated guide list with their ids.
      *  */
     function initGuideList() {
-        guides = ["doyou", "jiyoung"];
+        guides = [0, 1, 2, 3];
     }
     
     /**
@@ -49,15 +53,50 @@ $( document ).ready(function() {
      * search based on the current user typing inside input.
      */
     function onClickSearch() {
-        
+        // read entire database and match with country and city.
+        firebase.database().ref('/Guides/').once('value').then(function(snapshot) {
+            guides = [];
+            function match(left, right) {
+                var lowercase_right = right.charAt(0).toLowerCase() + right.slice(1);
+                return left == right || left == lowercase_right;
+            }
+            for (var i=0; i<snapshot.val().length; i++) {
+                if (match(searchInput.value, snapshot.val()[i].city) || match(searchInput.value, snapshot.val()[i].country)) {
+                    guides.push(snapshot.val()[i].id);
+                }
+            }
+            searchInput.value = '';
+            if (guides.length == 0) {
+                subtitle.innerHTML = 'Sorry, No Result Found...';
+            } else {
+                subtitle.innerHTML = 'Meet Recommended Local Guides!';
+            }
+            viewGuideList();
+        });
     }
     
-    function onClickAddQuest() {
-        
+    function onClickAddQuest(button, quest_id) {
+        button.className = 'btn btn-success btn-sm btn-block float-right';
+        button.innerHTML = 'Added';
+        button.onclick = function() {
+            onClickDeleteQuest(this, quest_id);
+        };
+
+        firebase.database().ref('/Quests/Quests/').once('value').then(function(snapshot) {
+            
+        });
     }
     
-    function onClickDeleteQuest() {
-    
+    function onClickDeleteQuest(button, quest_id) {
+        button.className = 'btn btn-outline-secondary btn-sm btn-block float-right';
+        button.innerHTML = 'Add Quest';
+        button.onclick = function() {
+            onClickAddQuest(this, quest_id);
+        };
+
+        firebase.database().ref('/Quests/Quests/').once('value').then(function(snapshot) {
+            
+        });
     }
     
     /**
@@ -77,35 +116,33 @@ $( document ).ready(function() {
      */
     function viewGuide(id) {
         // read guide information from database
-        firebase.database().ref(`/Guides/${id}/`).once('value').then(function(snapshot) {
+        firebase.database().ref(`/Guides/${id}/`).once('value').then(function(guideSnapshot) {
+        firebase.database().ref(`/Quests/Quests/`).once('value').then(function(questsSnapshot) {
             // data needed to view guide information
             // WARNING: how to download images properly?
-            var title = snapshot.val().title;
-            var carousel_images = ['1.png', '2.png', '3.png'];
-            var name = snapshot.val().name;
-            var country = snapshot.val().country;
-            var city = snapshot.val().city;
-            var profile_image = ['1.png'];
-            var profile_text = snapshot.val().profile_text;
-            // WARNING: get quest content based on quest_id
-            var quests = snapshot.val().quests;
+            var title = guideSnapshot.val().title;
+            var name = guideSnapshot.val().name;
+            var city = guideSnapshot.val().city;
+            var profile_text = guideSnapshot.val().profile_text;
+            var quests = guideSnapshot.val().quests;
+            var userQuests = questsSnapshot.val();
 
             // carousel indicators 
             var carousel_indicators = ``;
-            for (var i=0; i<carousel_images.length; i++) {
+            for (var i=0; i<carousel_images_count[id]; i++) {
                 if (i == 0) {
-                    carousel_indicators += `<li data-target='#${id}Carousel' data-slide-to='${i}' class='active'></li>`;
+                    carousel_indicators += `<li data-target='#Carousel${id}' data-slide-to='${i}' class='active'></li>`;
                 } else {
-                    carousel_indicators += `<li data-target='#${id}Carousel' data-slide-to='${i}'></li>`;
+                    carousel_indicators += `<li data-target='#Carousel${id}' data-slide-to='${i}'></li>`;
                 }
             }
             // carousel inner
             var carousel_inner = ``;
-            for (var i=0; i<carousel_images.length; i++) {
+            for (var i=0; i<carousel_images_count[id]; i++) {
                 if (i == 0) {
-                    carousel_inner += `<div class='carousel-item active'><img src='./static/img/${id}${i}.png' id='${id}${i}Image' class='d-block w-100' style='height:25%; overflow:hidden;'></div>`    
+                    carousel_inner += `<div class='carousel-item active'><img src='./static/img/${name}${i}.png' class='d-block w-100' style='height:25%; overflow:hidden;'></div>`    
                 } else {
-                    carousel_inner += `<div class='carousel-item'><img src='./static/img/${id}${i}.png' id='${id}${i}Image' class='d-block w-100' style='height:25%; overflow:hidden;'></div>`
+                    carousel_inner += `<div class='carousel-item'><img src='./static/img/${name}${i}.png' class='d-block w-100' style='height:25%; overflow:hidden;'></div>`
                 }
             }
             // quests
@@ -115,44 +152,43 @@ $( document ).ready(function() {
                 quests_inner += 
                 `<div class='row mt-3'> 
                     <div class='col-8'> 
-                        <li>${quests[i]}</li> 
+                        <li>${quests[i].quest_content}</li> 
                     </div> 
-                    <div class='col-4'> 
-                        <button class='btn btn-outline-secondary btn-sm float-right' type='button'>Add Quest</button> 
+                    <div class='col-4' id='addQuestButton${quests[i].quest_id}'> 
                     </div> 
                 </div>`;
             }
             
             // append the card to guide accordion innerHTML
             guideAccordion.innerHTML += 
-            `<div class='card' id='${id}Guide'> 
+            `<div class='card' id='Guide${id}'> 
                 <div class='card-title p-3 mb-0'> 
                 <h4> ${title} </h4> 
                 </div> 
-                <div id='${id}Carousel' class='carousel slide' data-ride='carousel'> 
+                <div id='Carousel${id}' class='carousel slide' data-ride='carousel'> 
                 <ol class='carousel-indicators'> 
                     ${carousel_indicators} 
                 </ol> 
                 <div class='carousel-inner'>
                     ${carousel_inner} 
-                    <a class='carousel-control-prev' href='#${id}Carousel' role='button' data-slide='prev'> 
+                    <a class='carousel-control-prev' href='#Carousel${id}' role='button' data-slide='prev'> 
                         <span class='carousel-control-prev-icon' aria-hidden='true'></span> 
                         <span class='sr-only'>Previous</span> 
                     </a> 
-                    <a class='carousel-control-next' href='#${id}Carousel' role='button' data-slide='next'> 
+                    <a class='carousel-control-next' href='#Carousel${id}' role='button' data-slide='next'> 
                         <span class='carousel-control-next-icon' aria-hidden='true'></span> 
                         <span class='sr-only'>Next</span> 
                     </a> 
                 </div> 
                 </div> 
-                <button class='btn' type='button' data-toggle='collapse' data-target='#${id}Collapse' aria-expanded='true' aria-controls='${id}Collapse'> 
+                <button class='btn' type='button' data-toggle='collapse' data-target='#Collapse${id}' aria-expanded='true' aria-controls='Collapse${id}'> 
                     <i class='fas fa-angle-down'></i> 
                 </button> 
                 <!-- profile of the guide --> 
-                <div id='${id}Collapse' class='collapse' aria-labelledby='${id}Guide' data-parent='#guideAccordion'> 
+                <div id='Collapse${id}' class='collapse' aria-labelledby='Guide${id}' data-parent='#guideAccordion'> 
                 <div class='card-body'> 
                     <div class='media'> 
-                    <img src='./static/img/${id}_profile.png' class='mr-2' style='width:40%; overflow:hidden;'>
+                    <img src='./static/img/${name}_profile.png' class='mr-2' style='width:40%; overflow:hidden;'>
                     <div class='media-body'>
                         <h6 class='mt-0'>
                         ${name}, ${city}
@@ -165,6 +201,45 @@ $( document ).ready(function() {
                     </div>
                 </div>
             </div>`;
+
+            // addQuest button
+            for (var i=0; i<quests.length; i++) {
+                var added = false;
+                let quest_id = quests[i].quest_id;
+                var button;
+                var div;
+                for (var j=0; j<userQuests.length; j++) {
+                    if (quest_id == userQuests[j].quest_id) {
+                        added = true;
+                    }
+                }
+                if (added) {
+                    button = document.createElement("button");
+                    button.className = 'btn btn-success btn-sm btn-block float-right';
+                    button.type = 'button';
+                    button.innerHTML = 'Added';
+
+                    button.onclick = function() {
+                        onClickDeleteQuest(this, quest_id);
+                    };
+
+                    div = document.getElementById(`addQuestButton${quest_id}`);
+                    div.appendChild(button);
+                } else {                    
+                    button = document.createElement("button");
+                    button.className = 'btn btn-outline-secondary btn-sm btn-block float-right';
+                    button.type = 'button';
+                    button.innerHTML = 'Add Quest';
+
+                    button.onclick = function() {
+                        onClickAddQuest(this, quest_id);
+                    };
+                    
+                    div = document.getElementById(`addQuestButton${quest_id}`);
+                    div.appendChild(button);
+                }
+            }
+        });
         });
     }
     
@@ -172,6 +247,7 @@ $( document ).ready(function() {
      * view guide list read from 'guides'. 
      */
     function viewGuideList() {
+        guideAccordion.innerHTML = ``;
         for (var i=0; i<guides.length; i++) {
             viewGuide(guides[i]);
         }
